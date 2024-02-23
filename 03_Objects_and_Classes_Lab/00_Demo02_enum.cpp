d04c54590a9cd6d84733b24a5008abd74a8f95d3
MZhikoV <m.zhikov@gmail.com>
2024-02-23 16:58:52 +0200

    Demo 03 solution

--- 03_Objects_and_Classes_Lab/00_Demo02_enum.cpp
+++ 03_Objects_and_Classes_Lab/00_Demo02_enum.cpp
@@ -16,7 +16,7 @@ int main() {
     enum planets {earth=3,mars,jupiter,saturn, uranus, neptune};
 
     //we can do maths with this enum, because it is just const int underneath
-    cout<<"earth ("<<earth<<") is"<<uranus-earth<<" planets away from uranus ("<<uranus<<")"<<endl;
+    cout<<"earth ("<<earth<<") is "<<uranus-earth<<" planets away from uranus ("<<uranus<<")"<<endl;
 
 
     enum class Planets {earth=3,mars,jupiter,saturn, uranus, neptune};
