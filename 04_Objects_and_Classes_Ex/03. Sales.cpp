2906fafe2ec5b2e930d16bcde13b521b4e848134
MZhikoV <m.zhikov@gmail.com>
2024-02-27 17:04:22 +0200

    Task 03 solution

--- 04_Objects_and_Classes_Ex/03. Sales.cpp
+++ 04_Objects_and_Classes_Ex/03. Sales.cpp
@@ -7,12 +7,64 @@
 #include <algorithm>
 #include <queue>
 #include <stack>
+#include <iomanip>
 
 using namespace std;
 
+typedef map<string,double> mmap;
+class Sale {
 
+    private:
+    string town;
+    string product;
+    double price;
+    double quantity;
+    double totalPrice;
+    mmap citySales;
+
+    public:
+
+    void setParameters(string & inpStr) {
+        istringstream input(inpStr);
+        string inp;
+        while (input>>inp) {
+            this->town=inp;
+            input>>this->product>>this->price>>this->quantity;
+            this->totalPrice=price*quantity;
+            citySales[town]+=totalPrice;
+        }
+    }
+
+    string getResult();
+
+};
+
+    string Sale::getResult(){
+        ostringstream ostr;
+        for (auto el:citySales) {
+            ostr<<el.first<<" -> "<<fixed<<setprecision(2)<<el.second<<endl;
+        }
+        return ostr.str();
+    }
 
 int main() {
+
+    int num;
+    cin>>num;
+    cin.ignore();
+
+    Sale sales;
+
+    while (num>0) {
+        string input;
+        getline(cin,input);
+        sales.setParameters(input);
+        num--;
+    }
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    cout<<sales.getResult()<<endl;
     
     
     system("pause");
