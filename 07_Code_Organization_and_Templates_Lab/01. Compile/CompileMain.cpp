3eddeda5a12158d14d78ec135eed29266c94538f
MZhikoV <m.zhikov@gmail.com>
2024-03-14 16:08:20 +0200

    Task 01 solution

---
+++ 07_Code_Organization_and_Templates_Lab/01. Compile/CompileMain.cpp
@@ -0,0 +1,15 @@
+#include "Defines.h"
+
+int main() {
+#ifndef DONT_COMPILE_THIS
+	this here has compilation errors :)
+#endif // !DONT_COMPILE_THIS
+	using STANDARD_TEMPLATE_LIBRARY;
+	string input;
+	cin >> input;
+	cout << input << endl;
+
+	system ("pause");
+
+	return 0;
+}
---
+++ 07_Code_Organization_and_Templates_Lab/01. Compile/Defines.h
@@ -0,0 +1,22 @@
+#define DONT_COMPILE_THIS
+
+#define STANDARD_TEMPLATE_LIBRARY namespace std
+
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+
+//using namespace std;
+
+
+
+#endif //!DEFINES_H
