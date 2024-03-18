046313019f538aa3655ff795a3e8369a6db3c4da
MZhikoV <m.zhikov@gmail.com>
2024-03-18 09:09:56 +0200

    Task 02 solution

---
+++ 08_OOP_Constructors_Lab/02. Notes/Skeleton/SolfegeNoteNaming.h
@@ -0,0 +1,40 @@
+#ifndef SOLFEGE_H
+#define SOLFEGE_H
+
+#include "NoteName.h"
+
+#include <string>
+
+
+class SolfegeNoteNaming {
+
+public:
+    NoteName operator() (const std::string & noteText) const {
+        if (noteText=="Do") {
+            return NoteName('C');
+        }
+        else if (noteText=="Re") {
+            return NoteName('D');
+        }
+        else if (noteText=="Mi") {
+            return NoteName('E');
+        }
+        else if (noteText=="Fa") {
+            return NoteName('F');
+        }
+        else if (noteText=="Sol") {
+            return NoteName('G');
+        }
+        else if (noteText=="La") {
+            return NoteName('A');
+        }
+        else if (noteText=="Si") {
+            return NoteName('B');
+        }
+        else  {
+            return NoteName('?');
+        }
+    }
+};
+
+#endif
