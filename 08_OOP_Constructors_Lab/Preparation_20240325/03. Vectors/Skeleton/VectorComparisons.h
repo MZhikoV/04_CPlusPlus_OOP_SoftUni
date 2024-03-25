11afc95bea17c7f990a3575da59a430918819e50
MZhikoV <m.zhikov@gmail.com>
2024-03-25 18:55:42 +0200

    Task 01 solution

--- 08_OOP_Constructors_Lab/Preparation_20240325/03. Vectors/Skeleton/VectorComparisons.h
+++ 08_OOP_Constructors_Lab/Preparation_20240325/03. Vectors/Skeleton/VectorComparisons.h
@@ -1,8 +1,34 @@
 #ifndef VECTOR_COMP_H
 #define VECTOR_COMP_H
 
+#include "Vector.h"
+
+struct VectorLengthComparer {
+bool operator < (const Vector & first, const Vector & second) const {
+    return t
+}
+
+};
 
 
+struct ReverseComparer {
+
+    public:
+     
+}
+
+
+
+/*
+int numVectors;
+	std::cin >> numVectors;
+
+	std::multiset<Vector, ReverseComparer<Vector, VectorLengthComparer> > vectors;
+	for (int i = 0; i < numVectors; i++) {
+		Vector v;
+		std::cin >> v;
+		vectors.insert(v);
+        */
 
 
 #endif
