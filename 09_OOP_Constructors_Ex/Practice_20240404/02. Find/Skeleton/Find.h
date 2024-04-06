638ae28f7c1337f6c71fbbb57740840db1f37d2c
MZhikoV <m.zhikov@gmail.com>
2024-04-06 07:57:16 +0300

    Task 02 Constructors Ex solution

--- 09_OOP_Constructors_Ex/Practice_20240404/02. Find/Skeleton/Find.h
+++ 09_OOP_Constructors_Ex/Practice_20240404/02. Find/Skeleton/Find.h
@@ -2,19 +2,27 @@
 #define FIND_H
 
 #include "Company.h"
-#include <vector> 
-#include <iostream>
 
-Company* find(std::vector<Company*> & companies, const int & searchID) {
+#include <vector>
 
-   // Company * result;
+
+
+Company * find(std::vector<Company*> & companies, const int & searchId) {
     std::vector<Company*>::iterator it1=companies.begin();
 
-    for ( ;it1!=companies.end();it1++ ) {
-        if ((*it1)->getId()==searchID) {
+    for (; it1!=companies.end(); it1++) {
+        if ((*it1)->getId()==searchId) {
             return *it1;
-        }  
         };
-        return nullptr;
-}
+    };
+    return nullptr;
+};
+
+
+/*string searchIdLine;
+	getline(cin, searchIdLine);
+	int searchId = stoi(searchIdLine);
+
+	Company* companyWithSearchedId = find(companies, searchId);
+    */
 #endif // !FIND_H
