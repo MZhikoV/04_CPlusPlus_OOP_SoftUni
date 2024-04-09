42437246921ebb0337d394a522135be909c7126b
MZhikoV <m.zhikov@gmail.com>
2024-04-09 17:20:03 +0300

    Task 05 solution

---
+++ 09_OOP_Constructors_Ex/Practice_20240404/05. Register/Skeleton/Register.cpp
@@ -0,0 +1,25 @@
+#include "Register.h"
+#include "Company.h"
+
+	Register::Register(size_t numCompanies):numAdded(0), companiesArray(new Company[numCompanies]) {};
+
+	void Register::add(const Company& c) {
+		companiesArray[numAdded++]=c;
+	};
+
+	Company Register::get(int companyId) const {
+		for (size_t idx=0; idx<numAdded;idx++) {
+			if (companiesArray[idx].getId()==companyId) {
+				return companiesArray[idx];
+			}
+		};
+		return Company();
+	};
+
+	Register::	~Register() {
+    delete[] companiesArray;
+}
+
+
+
+
